2b49165eb59ffbe65d83f2abe37f539107dcea71
lby0615 <69958011+lby0615@users.noreply.github.com>
2020-08-27 23:21:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 #clas notes
-print("Hello world!")
 name = input("Enter your name: ")
 
 print ("Hello, "+ name +"!")
 age = input("Enter your age: ")
 age = int(age)
-print (age+1)
 
-print ("You will be "+ str(age+1) + "next year")
-print(f"I am {age+1} next year")
+print ("You will be "+ str(age+1) + " next year")
