db3ce7493bcc4ee57d9485014e62a9c2a5140801
lby0615 <bjl5716@psu.edu>
2020-08-27 03:47:38 +0000

    delete hw

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ age = input("Enter your age: ")
 age = int(age)
 print (age+1)
 
-print ("You will be "+str(age+1)+ "next year.")
+print ("You will be "+str(age+1)+ " next year.")
 print(f"I am {age+1} next year")
-
-#homework
-degreec = float(input("Enter temperature in celsius: "))
-print (f"{degreec}°in Celsius is equivalent to {1.8*degreec+32}°Fahrenheit.")
